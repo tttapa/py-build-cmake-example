ae0c65b4e4965f67273dae1d37d29dcd97a3d721
Pieter Pas <pieter.p.dev@outlook.com>
2024-11-24 17:45:45 +0100

    Update CI, py-build-cmake, pybind11, pybind11-stubgen

--- cmake/Pybind11Stubgen.cmake
+++ cmake/Pybind11Stubgen.cmake
@@ -1,24 +1,50 @@
 function(pybind11_stubgen target)
 
-    find_package(Python3 REQUIRED COMPONENTS Interpreter)
-    add_custom_command(TARGET ${target} POST_BUILD
-        COMMAND ${Python3_EXECUTABLE} -m pybind11_stubgen
-                $<TARGET_FILE_BASE_NAME:${target}>
-                --bare-numpy-ndarray
-                --no-setup-py
-                -o $<TARGET_FILE_DIR:${target}>
-        WORKING_DIRECTORY $<TARGET_FILE_DIR:${target}>
-        USES_TERMINAL)
+    # Parse arguments
+    set(OPTIONS
+        # Package folder containing the Python extension module, relative to the
+        # installation prefix (CMAKE_INSTALL_PREFIX). This should match the
+        # DESTINATION argument of the given target.
+        PACKAGE
+        # Destination of the stubs. Should either be an absolute path, or a path
+        # relative to the PACKAGE folder.
+        DESTINATION
+        # The CMake installation component that the stub generation should be
+        # part of.
+        COMPONENT
+    )
+    cmake_parse_arguments(STUBGEN "" "${OPTIONS}" "" ${ARGN})
+    if (NOT DEFINED STUBGEN_COMPONENT)
+        set(STUBGEN_COMPONENT "python_stubs")
+    endif()
+    if (NOT DEFINED STUBGEN_PACKAGE)
+        set(STUBGEN_PACKAGE ${PY_BUILD_CMAKE_MODULE_NAME})
+    endif()
+    if (NOT DEFINED STUBGEN_DESTINATION)
+        set(STUBGEN_DESTINATION "")
+    endif()
 
-endfunction()
+    # Locate Python
+    if (NOT DEFINED Python3_EXECUTABLE)
+        find_package(Python3 REQUIRED COMPONENTS Interpreter)
+    endif()
 
-function(pybind11_stubgen_install target destination)
-
-    install(DIRECTORY
-        $<TARGET_FILE_DIR:${target}>/$<TARGET_FILE_BASE_NAME:${target}>-stubs/
-        EXCLUDE_FROM_ALL
-        COMPONENT python_stubs
-        DESTINATION ${destination}/$<TARGET_FILE_BASE_NAME:${target}>
-        FILES_MATCHING REGEX "\.pyi$")
+    # Run pybind11-stubgen in the installation prefix
+    set(STUBGEN_MOD $<TARGET_FILE_BASE_NAME:${target}>)
+    set(STUBGEN_DIR \"\${CMAKE_INSTALL_PREFIX}/${STUBGEN_PACKAGE}\")
+    set(STUBGEN_CMD "\"${Python3_EXECUTABLE}\" -m pybind11_stubgen
+        --exit-code -o \"${STUBGEN_DESTINATION}\" \"${STUBGEN_MOD}\"")
+    install(CODE "
+        message(STATUS \"Executing pybind11-stubgen for ${STUBGEN_MOD} \"
+                \"(destination: \\\"${STUBGEN_DESTINATION}\\\")\")
+        execute_process(COMMAND ${STUBGEN_CMD}
+                        WORKING_DIRECTORY ${STUBGEN_DIR}
+                        RESULT_VARIABLE STUBGEN_RET
+                        ECHO_OUTPUT_VARIABLE  ECHO_ERROR_VARIABLE
+                        COMMAND_ECHO STDERR)
+        if(NOT STUBGEN_RET EQUAL 0)
+            message(SEND_ERROR \"pybind11-stubgen ${STUBGEN_MOD} failed.\")
+        endif()
+        " EXCLUDE_FROM_ALL COMPONENT ${STUBGEN_COMPONENT})
 
 endfunction()
--- cmake/QueryPythonForPybind11.cmake
+++ cmake/QueryPythonForPybind11.cmake
@@ -3,11 +3,22 @@ mark_as_advanced(USE_GLOBAL_PYBIND11)
 
 # First tries to find Python 3, then tries to import the pybind11 module to
 # query the CMake config location, and finally imports pybind11 using
-# find_package(pybind11 REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH).
-function(find_pybind11_python_first)
-    find_package(Python3 MODULE REQUIRED COMPONENTS Interpreter Development.Module)
-    if (NOT USE_GLOBAL_PYBIND11)
-        # Query Python to see if it knows where the headers are
+# find_package(pybind11 ${ARGN} REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH),
+# where ${ARGN} are the arguments passed to this macro.
+macro(find_pybind11_python_first)
+
+    # https://github.com/pybind/pybind11/pull/5083
+    set(PYBIND11_USE_CROSSCOMPILING On)
+
+    # Find Python
+    if (CMAKE_CROSSCOMPILING AND NOT (APPLE AND "$ENV{CIBUILDWHEEL}" STREQUAL "1"))
+        find_package(Python3 REQUIRED COMPONENTS Development.Module)
+    else()
+        find_package(Python3 REQUIRED COMPONENTS Interpreter Development.Module)
+    endif()
+
+    # Query Python to see if it knows where the pybind11 root is
+    if (NOT USE_GLOBAL_PYBIND11 AND Python3_EXECUTABLE)
         if (NOT pybind11_ROOT OR NOT EXISTS ${pybind11_ROOT})
             message(STATUS "Detecting pybind11 CMake location")
             execute_process(COMMAND ${Python3_EXECUTABLE}
@@ -29,48 +40,4 @@ function(find_pybind11_python_first)
     # pybind11 is header-only, so finding a native version is fine
     find_package(pybind11 ${ARGN} REQUIRED CONFIG CMAKE_FIND_ROOT_PATH_BOTH)
 
-    # Tweak extension suffix when cross-compiling
-    if (CMAKE_CROSSCOMPILING)
-        if (NOT PY_BUILD_EXT_SUFFIX AND DEFINED ENV{SETUPTOOLS_EXT_SUFFIX})
-            set(PY_BUILD_EXT_SUFFIX $ENV{SETUPTOOLS_EXT_SUFFIX})
-        endif()
-        if (NOT PY_BUILD_EXT_SUFFIX)
-            message(STATUS "Determining Python extension suffix")
-            # Find the python3.x-config script in the sysroot instead of on the
-            # build system:
-            find_program(PY_BUILD_Python3_CONFIG
-                python${Python3_VERSION_MAJOR}.${Python3_VERSION_MINOR}-config
-                ONLY_CMAKE_FIND_ROOT_PATH)
-            # Report errors:
-            if (NOT PY_BUILD_Python3_CONFIG)
-                message(FATAL_ERROR "Unable to find python3-config."
-                    "\nTry manually setting PY_BUILD_EXT_SUFFIX.")
-            else()
-                # If we found the python3.x-config script, query it for the
-                # extension suffix:
-                execute_process(COMMAND ${PY_BUILD_Python3_CONFIG}
-                    --extension-suffix
-                    OUTPUT_VARIABLE PY_BUILD_EXT_SUFFIX
-                    ERROR_VARIABLE PY_BUILD_EXT_SUFFIX_ERR
-                    OUTPUT_STRIP_TRAILING_WHITESPACE
-                    RESULT_VARIABLE PY_BUILD_EXT_SUFFIX_RESULT)
-                # Report errors:
-                if (NOT PY_BUILD_EXT_SUFFIX_RESULT EQUAL 0
-                    OR NOT PY_BUILD_EXT_SUFFIX)
-                    message(FATAL_ERROR "Unable to determine extension suffix:"
-                        "\n${PY_BUILD_EXT_SUFFIX}"
-                        "\n${PY_BUILD_EXT_SUFFIX_ERR}"
-                        "\nTry manually setting PY_BUILD_EXT_SUFFIX.")
-                endif()
-                # Cache the result:
-                set(PY_BUILD_EXT_SUFFIX ${PY_BUILD_EXT_SUFFIX} CACHE STRING
-                    "The extension for Python extension modules")
-            endif()
-        endif()
-        # Override pybind11NewTools.cmake's PYTHON_MODULE_EXTENSION variable:
-        message(STATUS "Python extension suffix: ${PY_BUILD_EXT_SUFFIX}")
-        set(PYTHON_MODULE_EXTENSION ${PY_BUILD_EXT_SUFFIX}
-            CACHE INTERNAL "" FORCE)
-    endif()
-
-endfunction()
+endmacro()
