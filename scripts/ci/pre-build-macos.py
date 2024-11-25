1d5ff6389a1dce6c6a316f4ab6f7c8b8b90ead73
Pieter Pas <pieter.p.dev@outlook.com>
2024-11-25 11:47:32 +0100

    Don't enable cross compilation by default on macOS

--- scripts/ci/pre-build-macos.py
+++ scripts/ci/pre-build-macos.py
@@ -40,9 +40,10 @@ cpu_flags = {
         "-march=x86-64-v2",
     ],
 }[archs]
+
 cmake_opts = f"'CMAKE_OSX_ARCHITECTURES': '{';'.join(archs)}', "
 cmake_opts += f"'CMAKE_Fortran_FLAGS_INIT': '{' '.join(cpu_flags)}'"
-cross_profile = f"""\
+native_profile = f"""\
 include(default)
 [settings]
 arch={conan_arch}
@@ -55,11 +56,16 @@ tools.build:skip_test=True
 tools.build:cflags+={cpu_flags}
 tools.build:cxxflags+={cpu_flags}
 tools.cmake.cmaketoolchain:extra_variables={{{cmake_opts}}}
+"""
+cross_profile = native_profile
+cross_profile += f"""\
 tools.cmake.cmaketoolchain:system_name="Darwin"
 """
 
-Path("cibw.profile").write_text(cross_profile)
-print(cross_profile)
+cross = not can_run
+profile = cross_profile if cross else native_profile
+Path("cibw.profile").write_text(profile)
+print(profile)
 
 opts = dict(shell=True, check=True)
 run(
