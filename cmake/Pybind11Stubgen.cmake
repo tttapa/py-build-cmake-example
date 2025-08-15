7a2c4a90089d6b464d75de06e949ca74d3e3b27f
Pieter Pas <pieter.p.dev@outlook.com>
2025-08-15 17:01:00 +0200

    Improve pybind11-stubgen logic

--- cmake/Pybind11Stubgen.cmake
+++ cmake/Pybind11Stubgen.cmake
@@ -6,44 +6,33 @@ function(pybind11_stubgen target)
         # installation prefix (CMAKE_INSTALL_PREFIX). This should match the
         # DESTINATION argument of the given target.
         PACKAGE
-        # Destination of the stubs. Should either be an absolute path, or a path
-        # relative to the PACKAGE folder.
-        DESTINATION
         # The CMake installation component that the stub generation should be
         # part of.
         COMPONENT
     )
     cmake_parse_arguments(STUBGEN "" "${OPTIONS}" "" ${ARGN})
-    if (NOT DEFINED STUBGEN_COMPONENT)
-        set(STUBGEN_COMPONENT "python_stubs")
-    endif()
     if (NOT DEFINED STUBGEN_PACKAGE)
         set(STUBGEN_PACKAGE ${PY_BUILD_CMAKE_IMPORT_NAME})
     endif()
-    if (NOT DEFINED STUBGEN_DESTINATION)
-        set(STUBGEN_DESTINATION "")
+    if (NOT DEFINED STUBGEN_COMPONENT)
+        set(STUBGEN_COMPONENT "python_stubs")
     endif()
 
     # Locate Python
-    if (NOT DEFINED Python3_EXECUTABLE)
-        find_package(Python3 REQUIRED COMPONENTS Interpreter)
-    endif()
+    set(Python3_ARTIFACTS_PREFIX "_HOST")
+    find_package(Python3 REQUIRED COMPONENTS Interpreter)
 
     # Run pybind11-stubgen in the installation prefix
-    set(STUBGEN_MOD ${target})
-    set(STUBGEN_DIR \"\${CMAKE_INSTALL_PREFIX}/${STUBGEN_PACKAGE}\")
-    set(STUBGEN_CMD "\"${Python3_EXECUTABLE}\" -m pybind11_stubgen
-        --exit-code -o \"${STUBGEN_DESTINATION}\" \"${STUBGEN_MOD}\"")
+    set(STUBGEN_MODULE ${STUBGEN_PACKAGE}.$<TARGET_FILE_BASE_NAME:${target}>)
+    set(STUBGEN_CMD "\"${Python3_HOST_EXECUTABLE}\" -m pybind11_stubgen -o . --exit-code \"${STUBGEN_MODULE}\"")
     install(CODE "
-        message(STATUS \"Executing pybind11-stubgen for ${STUBGEN_MOD} \"
-                \"(destination: \\\"${STUBGEN_DESTINATION}\\\")\")
+        message(STATUS \"Executing pybind11-stubgen for ${STUBGEN_MODULE} \"
+                       \"(destination: \\\"\${CMAKE_INSTALL_PREFIX}\\\", interpreter: \\\"${Python3_HOST_EXECUTABLE}\\\")\")
         execute_process(COMMAND ${STUBGEN_CMD}
-                        WORKING_DIRECTORY ${STUBGEN_DIR}
-                        RESULT_VARIABLE STUBGEN_RET
-                        ECHO_OUTPUT_VARIABLE  ECHO_ERROR_VARIABLE
-                        COMMAND_ECHO STDERR)
+                        WORKING_DIRECTORY \"\${CMAKE_INSTALL_PREFIX}/${STUBGEN_DESTINATION}\"
+                        RESULT_VARIABLE STUBGEN_RET)
         if(NOT STUBGEN_RET EQUAL 0)
-            message(SEND_ERROR \"pybind11-stubgen ${STUBGEN_MOD} failed.\")
+            message(SEND_ERROR \"pybind11-stubgen ${STUBGEN_MODULE} failed.\")
         endif()
         " EXCLUDE_FROM_ALL COMPONENT ${STUBGEN_COMPONENT})
 
